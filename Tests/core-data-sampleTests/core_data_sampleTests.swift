fd6e764c3fcd5e8d77d6a86b1311181920f52125
Harikant Jammi <hjammi@allegion.com>
2024-07-08 17:24:30 +0530

    Initial Commit

---
+++ Tests/core-data-sampleTests/core_data_sampleTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import core_data_sample
+
+final class core_data_sampleTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
